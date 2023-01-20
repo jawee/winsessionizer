dba9ae38e8ca0615eeda8718f399cad2ee2214cc
Andreas Olsson <olsson.andreas@gmail.com>
2023-01-20 15:31:52 +0100

    filtering works. need to make it fuzzy

--- main.go
+++ main.go
@@ -8,41 +8,37 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type folder struct {
-    path string
-}
-
 type model struct {
-    choices []folder
-    filteredChoices []folder
+    choices []string
+    filteredChoices []string
     searchBox string
     cursor int
-    choice folder
+    choice string
 }
 
-func initialModel() model {
+func initialModel() *model {
     dirs := []string{"/home/figge/go/src/github.com/jawee","/home/figge/projects"}
-    folders := make([]folder,0)
+    folders := make([]string,0)
     for _, dir := range dirs {
         files, _ := os.ReadDir(dir)
         for _, f := range files {
             if f.IsDir() {
                 path := fmt.Sprintf("%s/%s", dir, f.Name())
-                folders = append(folders, folder{path})
+                folders = append(folders, path)
             }
         }
     }
-    return model{
+    return &model{
         choices: folders,
         filteredChoices: folders,
     }
 }
 
-func (m model) Init() tea.Cmd {
+func (m *model) Init() tea.Cmd {
     return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
     case tea.KeyMsg:
         switch msg.String() {
@@ -60,11 +56,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
             }
 
         case "enter":
-            //we're done. cd to selected one
             m.choice = m.filteredChoices[m.cursor]
             return m, tea.Quit
         case "backspace":
-            m.searchBox = m.searchBox[:len(m.searchBox)-1]
+            if len(m.searchBox) > 0 {
+                m.searchBox = m.searchBox[:len(m.searchBox)-1]
+                m.filter()
+            }
         default: 
             m.searchBox += msg.String()
             m.filter()
@@ -74,12 +72,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     return m, nil
 }
 
-func (m model) filter() {
+func (m *model) filter() {
     filterStr := m.searchBox
 
-    newArray := make([]folder, 0)
+    newArray := make([]string, 0)
     for _, s := range m.choices {
-        if strings.Contains(s.path, filterStr) {
+        if strings.Contains(s, filterStr) {
             newArray = append(newArray, s)
         }
     }
@@ -87,9 +85,9 @@ func (m model) filter() {
     m.filteredChoices = newArray
 }
 
-func (m model) View() string {
-    if m.choice.path != "" {
-        return fmt.Sprintf("%s\n", m.choice.path)
+func (m *model) View() string {
+    if m.choice != "" {
+        return fmt.Sprintf("%s\n", m.choice)
     }
     s := ""
     for i, choice := range m.filteredChoices {
@@ -98,7 +96,7 @@ func (m model) View() string {
             cursor = ">"
         }
 
-        s += fmt.Sprintf("%s %s\n", cursor, choice.path)
+        s += fmt.Sprintf("%s %s\n", cursor, choice)
     }
 
     s += fmt.Sprintf("Filter> %s", m.searchBox)
@@ -114,9 +112,9 @@ func main() {
         os.Exit(1)
     }
 
-    if m, ok := m.(model); ok && m.choice.path != "" {
+    if m, ok := m.(*model); ok && m.choice != "" {
         // fmt.Printf("cd to %s\n", m.choice.path)
-        fmt.Printf(m.choice.path)
+        fmt.Printf(m.choice)
         // os.Chdir(m.choice.path)
     }
 }
